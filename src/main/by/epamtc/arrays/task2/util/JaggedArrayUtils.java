87c59a98d25c28590d2d4dc07568181c56418681
maxiemar <denishicmihail@gmail.com>
2021-04-16 19:19:36 +0300

    Refactor

--- src/main/by/epamtc/arrays/task2/util/JaggedArrayUtils.java
+++ src/main/by/epamtc/arrays/task2/util/JaggedArrayUtils.java
@@ -15,41 +15,39 @@ public final class JaggedArrayUtils {
         return Integer.compare(sum1, sum2);
     };
     public static final Comparator<int[]> MAX_ELEMENT_COMPARATOR = (row1, row2) -> {
-        var indexOfMax1 = ArrayUtils.findIndexOfMax(row1);
-        var indexOfMax2 = ArrayUtils.findIndexOfMax(row2);
-        if (indexOfMax1.isEmpty() && indexOfMax2.isPresent()) {
-            return -1;
-        }
-        if (indexOfMax1.isPresent() && indexOfMax2.isEmpty()) {
-            return 1;
-        }
-        if (indexOfMax1.isPresent()) {
-            int min1 = row1[indexOfMax1.getAsInt()];
-            int min2 = row2[indexOfMax2.getAsInt()];
-            return Integer.compare(min1, min2);
+        var optionalIndexOfMax1 = ArrayUtils.findIndexOfMax(row1);
+        var optionalIndexOfMax2 = ArrayUtils.findIndexOfMax(row2);
+        if (optionalIndexOfMax1.isPresent() && optionalIndexOfMax2.isPresent()) {
+            int max1 = row1[optionalIndexOfMax1.getAsInt()];
+            int max2 = row2[optionalIndexOfMax2.getAsInt()];
+            return Integer.compare(max1, max2);
         } else {
-            return compareNullableRows(row1, row2);
+            return resolveNullEmptyRows(row1, row2);
         }
     };
     public static final Comparator<int[]> MIN_ELEMENT_COMPARATOR = (row1, row2) -> {
-        var indexOfMin1 = ArrayUtils.findIndexOfMin(row1);
-        var indexOfMin2 = ArrayUtils.findIndexOfMin(row2);
-        if (indexOfMin1.isEmpty() && indexOfMin2.isPresent()) {
-            return -1;
-        }
-        if (indexOfMin1.isPresent() && indexOfMin2.isEmpty()) {
-            return 1;
-        }
-        if (indexOfMin1.isPresent()) {
-            int min1 = row1[indexOfMin1.getAsInt()];
-            int min2 = row2[indexOfMin2.getAsInt()];
+        var optionalIndexOfMin1 = ArrayUtils.findIndexOfMin(row1);
+        var optionalIndexOfMin2 = ArrayUtils.findIndexOfMin(row2);
+        if (optionalIndexOfMin1.isPresent() && optionalIndexOfMin2.isPresent()) {
+            int min1 = row1[optionalIndexOfMin1.getAsInt()];
+            int min2 = row2[optionalIndexOfMin2.getAsInt()];
             return Integer.compare(min1, min2);
         } else {
-            return compareNullableRows(row1, row2);
+            return resolveNullEmptyRows(row1, row2);
         }
     };
 
     private JaggedArrayUtils() {
+    }
+
+    private static int resolveNullEmptyRows(int[] row1, int[] row2) {
+        if (row1 == null || row2 == null) {
+            return compareNullableRows(row1, row2);
+        }
+        if (row2.length == 0) {
+            return 1;
+        }
+        return -1;
     }
 
     private static int compareNullableRows(int[] nullableRow1, int[] nullableRow2) {
